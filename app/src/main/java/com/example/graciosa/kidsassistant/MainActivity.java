ed8d90e9852d8ee3681db491f966ca39f65a4a51
Guilherme Graciosa <graciosa@motorola.com>
2019-09-29 16:07:41 -0300

    New-work: Added light and dark modes, sensible to global settings, no app theme menu.

--- app/src/main/java/com/example/graciosa/kidsassistant/MainActivity.java
+++ app/src/main/java/com/example/graciosa/kidsassistant/MainActivity.java
@@ -45,19 +45,22 @@ import android.view.MenuItem;
  * 28. DONE - Use navigation for fragments management (https://codelabs.developers.google.com).
  * 29. DONE - Start computing time and launch 1st notification when feature is turned on.
  * 30. DONE - Display selected values in preference
- * 31. Use stacked bars in history to have in bars an indication of allowed and over played time.
- * 32. Security: broadcasts permissions, export, etc.
- * 33. Review typography adherence to material design (https://codelabs.developers.google.com).
- * 34. Replace expanded notification action with collapsed notification action (pause/play buttons)?
- * 35. Create a settings to enable/disable notification actions buttons?
- * 36. Internationalize and localize
- * 37. Clean: logs, commented code, not used code, not used resources, etc.
- * 38. Bug: invalid default shared preference (computePlayingTime=true, maxPlayingTime=120) is brought
+ * 31. DONE - Support light and dark modes, sensible to global settings, no app theme menu.
+ * 32. Use stacked bars in history to have in bars an indication of allowed and over played time.
+ * 33. Security: broadcasts permissions, export, etc.
+ * 34. Review typography adherence to material design (https://codelabs.developers.google.com).
+ * 35. Replace expanded notification action with collapsed notification action (pause/play buttons)?
+ * 36. Create a settings to enable/disable notification actions buttons?
+ * 37. Internationalize and localize
+ * 38. Clean: logs, commented code, not used code, not used resources, etc.
+ * 39. Bug: invalid default shared preference (computePlayingTime=true, maxPlayingTime=120) is brought
  *     when install App in my Z3 Play P multi user; cleaning app data from Global Settings solves the
  *     issue; issue does not happen in Z2 Play N single user neither G2 M single user.
- * 39. Add action to bar touch e.g. display data, playedTime and limit?
- * 40. Support light/dark/follow-system modes (https://developer.android.com/guide/topics/ui/look-and-feel/darktheme).
- * 41. Update database and notification when daily limit is changed in settings.
+ * 40. Add action to bar touch e.g. display data, playedTime and limit?
+ * 41. Add app theme menu light/dark/follow-system  (https://developer.android.com/guide/topics/ui/look-and-feel/darktheme).
+ * 42. Update database and notification when daily limit is changed in settings.
+ * 43. Change app bar text to white in light theme.
+ * 44. Add elevation to history charts.
  */
 
 public class MainActivity extends AppCompatActivity {
